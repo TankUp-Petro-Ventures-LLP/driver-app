11749cd0798f6a4a451f3ebee0b5b2998dcc2c8d
Shivam Sagar <shivam@tankup.co.in>
2020-06-27 19:16:12 +0530

    added upload slip option after very filling

--- src/configuration/config.ts
+++ src/configuration/config.ts
@@ -2,7 +2,7 @@ export class Config{
     static _API_URL : string = 'http://13.126.96.232:3000/api/';
 
     static API_URL = 'http://13.126.96.232:3000/api-v3';
-    // static API_URL = 'http://localhost:3000/api-v3';
+    // static API_URL = 'http://192.168.0.169:3000/api-v3';
 
     static METER_URL = (vehicle_id) => {
         return 'vehicle/'+vehicle_id+'/meter';
